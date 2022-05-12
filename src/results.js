416e573ed6575b6b8f64b4d426ded6f45950dce4
hokiepedro <hokie@outlook.com>
2022-05-12 12:35:49 -0400

    naming convention

--- src/results.js
+++ src/results.js
@@ -11,14 +11,14 @@ function Results(props) {
 		<output className="row">
 			<div className="col-sm-10 col-sm-offset-1 col-md-6 col-md-offset-0">
 				<div className="btn btn-block btn-info">
-					<p className="h4">{(Math.ceil(calculatedBAC) != 1) ? "You will need " + Math.ceil(calculatedBAC) + " goTenna X Devices" :
-						"You will need " + Math.ceil(calculatedBAC) + " goTenna X Device"} </p>
+					<p className="h4">{(Math.ceil(calculatedBAC) != 1) ? "You will need " + Math.ceil(calculatedBAC) + " goTenna X Pro devices" :
+						"You will need " + Math.ceil(calculatedBAC) + " goTenna Pro X device"} </p>
 				</div>
 			</div>
 			<div className="col-sm-10 col-sm-offset-1 col-md-6 col-md-offset-0">
 				<div className="btn btn-block btn-info">
-					<p className="h4">{(Math.ceil(calculatedBAC/30) != 1) ? "You will need " + Math.ceil(calculatedBAC/30) + " GoKits" :
-						"You will need " + Math.ceil(calculatedBAC/30) + " GoKit"}</p>
+					<p className="h4">{(Math.ceil(calculatedBAC/30) != 1) ? "You will need " + Math.ceil(calculatedBAC/30) + " goKits" :
+						"You will need " + Math.ceil(calculatedBAC/30) + " goKit"}</p>
 				</div>
 			</div>
 		</output>)

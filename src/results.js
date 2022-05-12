afcc5f713d79f0bd2c7e5c6b491fa0912d32d362
hokiepedro <hokie@outlook.com>
2022-05-12 10:47:52 -0400

    Update to include bundle

--- src/results.js
+++ src/results.js
@@ -16,7 +16,7 @@ function Results(props) {
 			</div>
 			<div className="col-sm-10 col-sm-offset-1 col-md-6 col-md-offset-0">
 				<div className="btn btn-block btn-info">
-					<p className="h4">You will neeed: {Math.ceil(calculatedBAC/30)} Gotenna X Devices </p>
+					<p className="h4">You will neeed: {Math.ceil(calculatedBAC/30)} goTenna X Devices </p>
 				</div>
 			</div>
 		</output>)
